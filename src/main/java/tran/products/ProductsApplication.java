f151a0f29576a996ef16b5267565a2d7c5ee752c
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-06-08 10:56:25 -0500

    Initial commit

---
+++ src/main/java/tran/products/ProductsApplication.java
@@ -0,0 +1,13 @@
+package tran.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApplication.class, args);
+	}
+
+}
